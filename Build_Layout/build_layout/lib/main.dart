e1a3b2e7d5720769d9dfb89fc4d14b9b5cea024f
Ellio <sereneheart888@gmail.com>
2024-07-21 10:44:54 +0300

    [A] Button wiht text

--- Build_Layout/build_layout/lib/main.dart
+++ Build_Layout/build_layout/lib/main.dart
@@ -86,3 +86,41 @@ class ButtonSection extends StatelessWidget {
     );
   }
 }
+
+class ButtonWithText extends StatelessWidget {
+  const ButtonWithText({
+    required this.color,
+    required this.icon,
+    required this.label,
+    super.key,
+  });
+
+  final Color color;
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          color: color,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
