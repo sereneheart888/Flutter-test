e21827ea07c67d6699eba604059a0d6cbd32bcc0
Ellio <sereneheart888@gmail.com>
2024-07-21 11:15:39 +0300

    [A] Image section

--- Build_Layout/build_layout/lib/main.dart
+++ Build_Layout/build_layout/lib/main.dart
@@ -20,6 +20,9 @@ class MyApp extends StatelessWidget {
         body: const SingleChildScrollView(
           child: Column(
             children: [
+              ImageSection(
+                image: 'images/lake.jpg',
+              ),
               TitleSection(
                 name: 'Oeschinen Lake Campground',
                 location: 'Kandersteg, Switzerland',
@@ -148,6 +151,22 @@ class TextSection extends StatelessWidget {
         description,
         softWrap: true,
       ),
+    );
+  }
+}
+
+class ImageSection extends StatelessWidget {
+  const ImageSection({super.key, required this.image});
+
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      image,
+      width: 600,
+      height: 240,
+      fit: BoxFit.cover,
     );
   }
 }
