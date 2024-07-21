57fdbbe1dbc58bb8a5797f2b1ada79e63a5b40e8
Ellio <sereneheart888@gmail.com>
2024-07-21 12:43:21 +0300

    [A] favourite figure

--- Build_Layout/build_layout/lib/main.dart
+++ Build_Layout/build_layout/lib/main.dart
@@ -70,12 +70,11 @@ class TitleSection extends StatelessWidget {
               ],
             ),
           ),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          const SizedBox(width: 32),
-          const Text('41')
+          // Icon(
+          //   Icons.star,
+          //   color: Colors.red[500],
+          // ),
+          const FavoriteWidget(),
         ],
       ),
     );
@@ -169,4 +168,63 @@ class ImageSection extends StatelessWidget {
       fit: BoxFit.cover,
     );
   }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  const FavoriteWidget({super.key});
+
+  @override
+  State<FavoriteWidget> createState() => _FavoriteWidgetState();
+}
+// #enddocregion favorite-widget
+
+// #docregion favorite-state, favorite-state-fields, favorite-state-build
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  // #enddocregion favorite-state-build
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+  // #enddocregion favorite-state-fields
+
+  // #docregion toggle-favorite
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+  // #enddocregion toggle-favorite
+
+  // #docregion favorite-state-build
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            padding: const EdgeInsets.all(0),
+            alignment: Alignment.center,
+            icon: (_isFavorited
+                ? const Icon(Icons.star)
+                : const Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: SizedBox(
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ],
+    );
+  }
+  // #docregion favorite-state-fields
 }
